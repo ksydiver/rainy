e7e76f9f33cbbc8cd07665e985f60094fad41600
Ксения Черотченко <kseniacherotchenko@MacBook-Pro-Ksenia.local>
2019-06-22 23:22:26 +0300

    Updates

---
+++ precache-manifest.25db7e863fb7e20bd6159a7751e522d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31cc609cba7b0615521a315138e788e",
+    "url": "/rainy/index.html"
+  },
+  {
+    "revision": "8fa50fa57d4486bfc4c8",
+    "url": "/rainy/static/css/2.7df38a7a.chunk.css"
+  },
+  {
+    "revision": "89e77f6be454e480b32d",
+    "url": "/rainy/static/css/main.2d6ddef7.chunk.css"
+  },
+  {
+    "revision": "8fa50fa57d4486bfc4c8",
+    "url": "/rainy/static/js/2.f0f4c907.chunk.js"
+  },
+  {
+    "revision": "89e77f6be454e480b32d",
+    "url": "/rainy/static/js/main.42544d71.chunk.js"
+  },
+  {
+    "revision": "6ffc816de377a99de3de",
+    "url": "/rainy/static/js/runtime~main.e3e24468.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/rainy/static/media/persik.4e1ec840.png"
+  }
+]);
